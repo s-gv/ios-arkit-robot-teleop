7b72dee99579041fe2de2d0611e311cb53e83695
Sagar Gubbi <sagar.writeme@gmail.com>
2020-05-03 22:14:32 +0530

    Teleoperate Dobot

---
+++ README.md
@@ -0,0 +1,13 @@
+dobot-arkit-teleop
+==================
+
+Teleoperate a Dobot Magician using the iPhone. This uses the Augmented Reality (ARKit) tools in iOS to track the 3D position of the iOS device in the room and moves the Dobot arm in lockstep.
+
+Requirements
+------------
+
+- Python 3.6
+- iOS >= 13
+- Xcode 11.4
+- pydobot 1.1.0
+
--- dobot_ios_teleop.py
+++ dobot_ios_teleop.py
@@ -14,9 +14,9 @@ d.wait_for_cmd(d.grip(False))
 
 g_state = False
 
-d_orig_x = -170.
-d_orig_y = 122.
-d_orig_z = 60.
+d_origin_x = -170.
+d_origin_y = 122.
+d_origin_z = 60.
 dr = -20
 
 d_scale = 10.
@@ -29,15 +29,25 @@ try:
         ix, iy, iz, ig = float(vals[0]), float(vals[1]), float(vals[2]), 'r' in vals[3]
         print(ix, iy, iz, ig)
 
-        dx = d_orig_x + d_scale * ix
-        dy = d_orig_y + d_scale * iy
-        dz = d_orig_z + d_scale * iz
+        dx = d_origin_x + d_scale * ix
+        dy = d_origin_y + d_scale * iz
+        dz = d_origin_z + d_scale * iy
         '''
-        d.wait_for_cmd(d.move_to(dx, dy, dz, dr))
+        if dx > -180 and dx < 180 and dy > -180 and dy < 180 and dz > -10 and dz < 60:
+            d.wait_for_cmd(d.move_to(dx, dy, dz, dr))
         if ig != g_state:
             d.wait_for_cmd(d.grip(g_state))
             g_state = ig
         '''
+
+        # Read and discard any buffered samples
+        sock.setblocking(0)
+        while True:
+            try:
+                _, _ = sock.recvfrom(1024)
+            except BlockingIOError:
+                break
+        sock.setblocking(1)
 except KeyboardInterrupt:
     print('Interrupted...')
 
